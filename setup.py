bc1db8d21bc213b91421ba1dab410ead0c831c98
Dmitrii.Proskurin <dmitrii.proskurin@jetbrains.com>
2021-06-19 01:24:11 +0300

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
    name = "Example",
    install_requires=[
-      "numpy >= 1.15.0",
-      "numba >= 0.43.0"
+	"numba >= 0.43.0",
+	"django_html_comments >= 0.3.1",
+	"mosaik >= 3.0.0"
    ]
 )
